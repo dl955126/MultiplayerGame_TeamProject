f35ac8870a98cf0511ece0c16102e49ee5067ba6
Blackcat-gif <157761600+Blackcat-gif@users.noreply.github.com>
2025-10-21 21:03:54 -0500

    prototype of level, UI, and powerup

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
     public GameObject player;
+    public float PowerupTime;
+    void Start()
+    {
+        PowerupTime = 0;
+    }
+    void Update()
+    {
+        PowerupTime -= Time.deltaTime;
+    }
 
     private void OnCollisionEnter(Collision c1)
     {
@@ -11,6 +20,7 @@ public class PowerUp : MonoBehaviour
         {
 
             gameObject.SendMessage("PowerUp", "example");
+            Destroy(gameObject);
         }
     }
 
